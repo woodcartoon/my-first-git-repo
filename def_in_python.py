9b41209c5df821ac8fc8d3f867729b8c3982dd56
tsako ubise <tsakoubise@mail.com>
2025-07-18 15:43:10 +0200

    print each result in a new line

--- def_in_python.py
+++ def_in_python.py
@@ -5,15 +5,15 @@ digit_two = int(input("Enter the second digit: "))
 def add_two_number(x, y):
     # this function adds, multiplies and divides two numbers
     sum = x + y
+    addition = print(sum)
     multiplication = x * y
+    times = print(multiplication)
     division = x/y
+    div = print(division)
 
-    return sum, multiplication, division
+    return addition, times, div
 
 
 # in this code below i am calling this function
-# this returns each  of the result in a next line
+# this code will print each result in a new line
 add_two_number(digit_one, digit_two)
-
-# this prints the results in a tupple set
-print(add_two_number(digit_one, digit_two))
