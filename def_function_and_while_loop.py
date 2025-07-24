46e2d56e07aea35174fb96daff856d3c0c1816c9
tsako ubise <tsakoubise@mail.com>
2025-07-24 15:47:41 +0200

    Implement greet function and while loop for user interaction

---
+++ def_function_and_while_loop.py
@@ -0,0 +1,18 @@
+def greet(string):
+    print(f"hello {string} , we are delighted to have you")
+
+
+greet("Tsako Ubise")
+greet("Max verstepan")
+greet("John wick")
+# what I have learned about defining functions is that is a set of instructions that can be reused like i just did above .
+
+# or you can use a while loop
+
+
+count = 0
+while count < 3:
+    name = input("enter your name: ")
+    print(f"greetings {name}")
+
+    count += 1
