7b58f03738f3db6d5648aa2f7200bad1846522fe
tsako ubise <tsakoubise@mail.com>
2025-07-24 15:42:04 +0200

    I have successfully written a code and it run okay

---
+++ string_reverse.py
@@ -0,0 +1,10 @@
+def reverse(string):
+    word = string[::-1]
+
+    print(word)
+
+
+reverse("hello world")
+reverse("max ubise")
+
+# this function is used to print a string to be written in reverse
