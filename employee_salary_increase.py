ffd96ccfa96558fd88645b3fa4a1461432515822
tsako ubise <tsakoubise@mail.com>
2025-07-24 16:04:50 +0200

    Refactor employee_salary_increase.py to improve code structure and readability

--- employee_salary_increase.py
+++ employee_salary_increase.py
@@ -1,1 +1,15 @@
+# this file is used to increase the salary of an employee by a certain percentage for three employees
 
+count = 0
+
+while count < 3:
+    salary = int(input("please enter your salary: "))
+    increase = int(input("please enter the percentage increase: "))
+
+    def increase_salary(salary, increase):
+
+        new_salary = salary + (salary * increase / 100)
+        print(f"Your new salary is: R{new_salary}")
+        return new_salary
+    increase_salary(salary, increase)
+    count += 1
